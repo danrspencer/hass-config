8e05a3b7ea223c4b6f0062673be572ccbe4c62db
Dan Spencer <danrspen@gmail.com>
2017-07-04 22:00:43 +0100

    Added some more sensors and updated scripts

--- home-assistant/scripts/check-config.sh
+++ home-assistant/scripts/check-config.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-echo
-echo -e "\033[1;34mCopying config... \033[0m"
-sudo cp ./config/*.yaml /home/homeassistant/.homeassistant/
-
-echo
 echo -e "\033[1;34mChecking config... \033[0m"
 sudo su -s /bin/bash homeassistant -c \
   "source /srv/homeassistant/bin/activate \
