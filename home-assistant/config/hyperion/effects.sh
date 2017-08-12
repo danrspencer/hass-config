8eb227201567ea8874f0c193f645eda423d21077
Dan Spencer <danrspen@gmail.com>
2017-08-12 19:35:09 +0100

    Tweaked some automations and improved media player stuff

--- home-assistant/config/hyperion/effects.sh
+++ home-assistant/config/hyperion/effects.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "{\"effect\": {\"name\": \"Green mood blobs\"}, \"command\": \"effect\", \"priority\": 100 }" | nc 192.168.0.27 19444
+echo "{\"effect\": {\"name\": \"$1\"}, \"command\": \"effect\", \"priority\": 100 }" | nc 192.168.0.27 19444
