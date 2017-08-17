cf45a18c1055cb95c58fbb9e08d526128b485b60
Dan Spencer <danrspen@gmail.com>
2017-08-17 10:26:08 +0100

    Moved all the config up to the top level

--- home-assistant/scripts/check-config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo -e "\033[1;34mChecking config... \033[0m"
-sudo su -s /bin/bash homeassistant -c \
-  "source /srv/homeassistant/bin/activate \
-  && hass --script check_config"
