09ea3f6b1e17403a0b0ca72e4648d29340306e01
Dan Spencer <danrspen@gmail.com>
2017-08-18 16:31:21 +0100

    Added some logging to the git shell script

--- bash_scripts/git_pull.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Resetting to origin/master..."
-git reset --hard origin/master
-
-echo "Pulling..."
-git pull
---
+++ bash_scripts/update_from_git.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+date >> /config/update_from_git.log
+git reset --hard origin/master >> /config/update_from_git.log
+git pull >> /config/update_from_git.log
