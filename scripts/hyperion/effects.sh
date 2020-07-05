64f7aafddc86533f341764edebabd4f3bb927d58
Dan Spencer <danrspen@gmail.com>
2020-07-05 19:30:33 +0000

    Added in the illuminance sensor

--- scripts/hyperion/effects.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo '{ "command": "clearall"}' | nc 192.168.0.27 19444
-echo "{\"effect\": {\"name\": \"$1\"}, \"command\": \"effect\", \"priority\": 100 }" | nc 192.168.0.27 19444
